5f85b419e59f69358b2ac209671d82a1a7eb9c5f
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2020-02-19 11:29:54 +0300

    More tests for global object and properties

--- compiler/testData/diagnostics/nativeTests/topLevelSingleton.kt
+++ compiler/testData/diagnostics/nativeTests/topLevelSingleton.kt
@@ -1,4 +1,13 @@
+// FILE: annotation.kt
+package kotlin.native.concurrent
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.BINARY)
+annotation class ThreadLocal
+
 // FILE: test.kt
+import kotlin.native.concurrent.ThreadLocal
+
 class AtomicInt(var value: Int)
 object Foo {
     <!VARIABLE_IN_TOP_LEVEL_SINGLETON_WITHOUT_THERAD_LOCAL!>var field1: Int = 10<!>
@@ -7,3 +16,11 @@ object Foo {
         get() = backer2.value
         set(value: Int) { backer2.value = value }
 }
+
+@ThreadLocal
+object Bar {
+    var field1: Int = 10
+    var field2: String? = null
+}
+
+var topLevelProperty = "Global var"
